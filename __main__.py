2a12eb105216af7a6e2b3a02bc8c785ff8b9ff1f
Zephyr Embyr <zephyrembyr@gmail.com>
2023-12-07 18:53:01 -0600

    Update __main__.py

--- __main__.py
+++ __main__.py
@@ -2,10 +2,10 @@ from PaperMario.src.gui import GUI
 import PaperMario.tests.test_performance as tests
 
 if __name__ == '__main__':
-	# gui = GUI()
-	# gui.run()
+	gui = GUI()
+	gui.run()
 
-	tests.testSearchPerformance()
+	# tests.testSearchPerformance()
 
 
 	
